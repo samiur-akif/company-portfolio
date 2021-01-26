3b28955f963a99dbb1d638d6213fa5bc3f406f70
Sabbir Rifat <rifat234dgh@gmail.com>
2021-01-26 20:21:29 +0600

    Completed the homepage and homepage backend and navbar

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '18587cb96d07c21cfc8b04bf2a425508'),
+    },
+  },
+});
---
+++ server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd56a9457-b561-4864-8789-fd36b41a8adb'
+};
